fc8e8d74eda773841beee51b0010b9c137563e34
1ucky40nc3 <louisnwendler@gmail.com>
2022-06-10 14:49:18 +0200

    upped numbers of instant-ngp nerf

--- run_nerf.py
+++ run_nerf.py
@@ -269,16 +269,16 @@ def create_instantngp(args):
             "otype": "HashGrid",
             "n_levels": 16,
             "n_features_per_level": 2,
-            "log2_hashmap_size": 15,
+            "log2_hashmap_size": 19,
             "base_resolution": 16,
-            "per_level_scale": 1.5
+            "per_level_scale": 2.0
         },
         "network_config": {
             "otype": "FullyFusedMLP",
             "activation": "ReLU",
             "output_activation": "None",
             "n_neurons": 64,
-            "n_hidden_layers": 2
+            "n_hidden_layers": 6
         }
     }
     model = tcnn.NetworkWithInputEncoding(
