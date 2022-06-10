ea1f0b02840485df15667ee7c0002c17a3b165a4
1ucky40nc3 <louisnwendler@gmail.com>
2022-06-10 18:14:22 +0200

    cleaned code a little

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -50,16 +50,16 @@ def get_embedder(multires, i=0):
         return nn.Identity(), 3
     
     embed_kwargs = {
-                'include_input' : True,
-                'input_dims' : 3,
-                'max_freq_log2' : multires-1,
-                'num_freqs' : multires,
-                'log_sampling' : True,
-                'periodic_fns' : [torch.sin, torch.cos],
+        'include_input' : True,
+        'input_dims' : 3,
+        'max_freq_log2' : multires - 1,
+        'num_freqs' : multires,
+        'log_sampling' : True,
+        'periodic_fns' : [torch.sin, torch.cos],
     }
     
     embedder_obj = Embedder(**embed_kwargs)
-    embed = lambda x, eo=embedder_obj : eo.embed(x)
+    embed = lambda x, eo=embedder_obj: eo.embed(x)
     return embed, embedder_obj.out_dim
 
 
